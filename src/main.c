718e32c6cab5c92ec8684e1c5c1ab7e3c60f4938
FreakyF <kamil.fudala44@gmail.com>
2022-06-02 22:36:31 +0200

    Setup project

--- src/main.c
+++ src/main.c
@@ -1,6 +1,29 @@
-#include <stdio.h>
+#include <SDL.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+#define WIDTH 640
+#define HEIGHT 480
+
+int main(int argc, char *argv[]) {
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    SDL_Window *window = SDL_CreateWindow("Hello SDL World", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH,
+                                          HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
+
+    if (NULL == window) {
+        return 1;
+    }
+
+    SDL_Event windowEvent;
+    while (1) {
+        if (SDL_PollEvent(&windowEvent)) {
+            if (SDL_QUIT == windowEvent.type) {
+                break;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
 }
